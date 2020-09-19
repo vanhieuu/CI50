00eab92e39db536a7d0d4a7f3bab5694ddf6029c
Vhieu <vanhieuu99@gmail.com>
2020-09-19 20:33:02 +0700

    Session 2 - chữa bài vào OOP

---
+++ Session2/app.js
@@ -0,0 +1,24 @@
+
+            //Chữa bài tập về nhà  
+// let input = prompt("input your numbers ");
+
+// let arr = input.split(",").map((str) => {
+//     return Number(str);
+// });
+
+// let result = [];
+//     for(let i = 0; i < arr.length - 1; i++){
+//         let count = 0 ;
+
+//             for (let j = i + 1; j < arr.length; j++) {
+                   
+//                         if( arr[i] == arr[j]) {
+//                                 count++;
+//                         }
+//                     }    
+//                     if( count == 0){
+//                         result.push(arr[i]);
+//                     }
+//     }
+//     console.log(result);
+
--- buoi1(off)/app.js
+++ buoi1(off)/app.js
@@ -1,7 +1,10 @@
-let $arr = [1,1,2,3,4,5]
-$arr.forEach((num,i) => {
-        if($arr.indexOf(num != i)){
-            $arr = $arr.filter(numTemp => num != numTemp)
-        }
-})
+let $arr = [1,1,2,3,4,5];
+let $result = [];
+// $arr.forEach((num,i) => {
+//         if($arr.indexOf(num != i)){
+//             $arr = $arr.filter(numTemp => num != numTemp)
+//         }
+// })
+
+
 console.log($arr);
